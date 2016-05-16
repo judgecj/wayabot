86387e4757e60741e61b1ff4d8db8a945d40a676
andela-oagboola <olaide.agboola@andela.co>
2016-05-16 22:29:32 +0100

    update user profile

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -19,6 +19,7 @@ Template.profile.events({
     if(username != Meteor.user().username){
       Meteor.call('changeUserName', username);
     }
+<<<<<<< HEAD
   },
   "click .update-email": function(event, template){
     var newMail = template.$('.sign-email').val();
@@ -57,4 +58,7 @@ Template.profile.events({
 Accounts.onResetPasswordLink(function(token, done){
   console.log('token', token);
   Router.go('/reset-password')
+=======
+  }
+>>>>>>> update user profile
 });
