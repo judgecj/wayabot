cccfc55903bb09ff959e300501123e9eddcb5c4e
andela-ysanni <yetunde.sanni@andela.com>
2016-05-18 23:06:36 +0100

    debugging production

--- client/home/home.js
+++ client/home/home.js
@@ -48,7 +48,9 @@
 // })
 
 Accounts.onEmailVerificationLink(function(token, done){
+	console.log('token from email', token);
   Accounts.verifyEmail(token, function(err){
+  	console.log('verifying email', err);
     if(err){
       console.log('error', err);
     }
